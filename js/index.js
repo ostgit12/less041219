5d7cbbe9cd01c353c7501b79f732268aa9bef738
ostgit12 <zostap@ymail.com>
2019-12-04 17:22:44 +0000

    correct

---
+++ js/index.js
@@ -0,0 +1,45 @@
+//декларуємо змінні:
+//var-старе, не використовуємо
+//let-звичайна змінна
+//const-стала змінна
+
+//primitive data types
+//Number -
+//String -
+//Boolean -
+//null - пусто
+//undefined -
+//якщо ств змінна але нічого незаписато то це undefined
+//для очистки змін потрібно = null
+//Object - обєкт
+//Symbol - символ унікальний незмінний ідентифікатор
+
+
+
+let bestGreetings = 'Hello';
+alert(bestGreetings);
+bestGreetings = 'Java Script';
+alert(bestGreetings);
+
+const age = Number(prompt('Enter your age'));
+
+alert('Your age is '+age);
+
+const nextAge = addOne(age);
+
+alert('Next year you will be: '+nextAge);
+
+//logical operators
+//> < >= <= != == === || &&
+if(age>=18){
+    alert('you are adult');
+}
+else{
+    alert('you are underage');
+
+}
+
+function addOne(num){
+    const incremented = num + 1;
+    return incremented;
+}
